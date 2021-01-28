6d98640a29bbe15ed1224af794db752febd4b898
Oriqe <60728090+Oriqe@users.noreply.github.com>
2021-01-28 17:45:15 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,26 +3,21 @@ var router = express.Router();
 const Scrape_all = require("./resources/scraping_functionalities.js")
 
 
-console.log(Scrape_all)
+// simple explaination page
 router.get("/", function(req,res){
     res.sendFile(__dirname + "\\views\\index.html")
 })
 
-router.get("/video_id=:videoId/max_comments=:maxComments", function(req, res) {
-    
-    ( async () => {
-        
-        const scraper = new Scrape_all(req.params["videoId"], req.params["maxComments"])
-        console.log("Please wait while comments and replies are being fetched.")
-        let new_ans = await scraper.starter()
 
-        console.log(new_ans);
-        res.render((__dirname + "\\views\\comment_dump.pug"), {comment_dump: new_ans});
-        
-        
+// url end point 
+router.get("/video_id=:videoId/max_comments=:maxComments", function(req, res) {
+    // creating a new scraping instance and sending the main list and reply-count to the .pug file
+    ( async () => {
+        const scraper = new Scrape_all(req.params["videoId"], req.params["maxComments"])
+        console.log(`Please wait while comments and replies are being fetched. worker: ${process.pid}`)
+        const [new_ans, total_replies] = await scraper.starter()
+        res.render((__dirname + "\\views\\comment_dump.pug"), {comment_dump: new_ans, reply_count: total_replies});
      })();
-    
-    
 })
 
   
