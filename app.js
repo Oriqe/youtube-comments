4af07fa0c4ce388bc58d0c80b35913bdae7423b2
Oriqe <60728090+Oriqe@users.noreply.github.com>
2021-01-28 16:02:01 +0200

    Update app.js
    adding a hint of horizontal scaling.

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-var express = require("express");
-var path = require("path");
-var routes = require("./routes")
-var app = express();
+const express = require("express");
+const path = require("path");
+const routes = require("./routes")
+const cluster = require('cluster');
 
+const app = express();
 app.set("port", process.env.PORT || 3000);
-
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, 'resources')))
 app.set("view engine", "pug")
-
 app.use(routes)
 
 
+const numCPUs = require('os').cpus().length
 
-app.listen(app.get("port"), function(){
-  console.log('The server is available at http://127.0.0.1:3000/')
 
-})
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+
+  app.listen(app.get("port"), function(){
+    console.log('The server is available at http://127.0.0.1:3000/')
+    console.log(`Worker ${process.pid} started`)
+}
+  )}
+
